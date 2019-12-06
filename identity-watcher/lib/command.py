b7d8829e584cc6838154f34353946bb0eb3be0f0
Ike The Coder <ikethecoder@canzea.com>
2019-12-06 11:49:13 -0800

    Fix handling of single command arg

--- identity-watcher/lib/command.py
+++ identity-watcher/lib/command.py
@@ -5,6 +5,9 @@ log = logging.getLogger(__name__)
 
 def call(*args):
     log.debug("CMD %s" % str(args))
-    output = subprocess.check_output(args, stderr=subprocess.STDOUT)
+    cmd = args
+    if len(args) == 1:
+        cmd = args[0].split(' ')
+    output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
     for line in output.split(b'\n'):
         log.debug("CMD   %s" % line.decode("utf-8"))
