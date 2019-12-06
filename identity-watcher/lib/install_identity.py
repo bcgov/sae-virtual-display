33dfef62de7be53ff6771710a4efa63ba1838549
Ike The Coder <ikethecoder@canzea.com>
2019-12-06 15:11:12 -0800

    Fix install for identity watcher

--- identity-watcher/lib/install_identity.py
+++ identity-watcher/lib/install_identity.py
@@ -5,27 +5,31 @@ from command import call
 
 log = logging.getLogger(__name__)
 
+def encoded_content (data):
+    return base64.b64encode(data.encode('utf-8')).decode('utf-8')
+
 def install_files(secret_data):
 
     log.info("Writing Minio details..")
     with open("/tmp-auth-minio/config.json", "w") as f:
         log.info("WRITE /tmp-auth-minio/config.json")
-        f.write(base64.b64encode(secret_data["mc-config.json"]).encode('utf-8'))
+        f.write(encoded_content(secret_data["mc-config.json"]))
 
     log.info("Writing Postgres details..")
     for k in secret_data.keys():
         if k.startswith("postgresql."):
             with open("/tmp-pki-postgres/%s" % k, "w") as f:
                 log.info("WRITE /tmp-pki-postgres/%s" % k)
-                f.write(base64.b64encode(secret_data[k]).encode('utf-8'))
+                f.write(encoded_content(secret_data[k]))
 
     log.info("Writing Browser NSSDB details..")
     for k in secret_data.keys():
         if not (k.startswith("postgresql.") or k == "refresh_token"):
             with open("/tmp-pki-nssdb/%s" % k, "w") as f:
                 log.info("WRITE /tmp-pki-nssdb/%s" % k)
-                f.write(base64.b64encode(secret_data[k]).encode('utf-8'))
+                f.write(encoded_content(secret_data[k]))
 
+    log.info("Setting access..")
     access = [
         "chown -R 1000:1000 /tmp-auth-minio/*",
         "chmod -R 0700 /tmp-auth-minio/*",
