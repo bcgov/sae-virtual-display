6a4e44e0843003b1f74cb8c23aa35b67cb95f7f6
Ike The Coder <ikethecoder@canzea.com>
2019-12-07 11:03:49 -0800

    Identity watcher logging

--- identity-watcher/lib/watcher.py
+++ identity-watcher/lib/watcher.py
@@ -35,32 +35,39 @@ else:
     jwt = json.loads(jwt_str)
     refresh_token = jwt['refresh_token']
 
+errors = 0
+
 while True:
 
-    try:
-        log.info("--")
-        log.info("--")
-        log.info("--")
-        log.info("Check Identity...")
-        gen = GenIdentity()
+    if errors == 10:
+        log.error("Too many errors.. turning off identity watcher.")
+        sleep_time = 60*60*4 # 4 hr
+    else:
+        try:
+            log.info("--")
+            log.info("--")
+            log.info("--")
+            log.info("Check Identity...")
+            gen = GenIdentity()
 
-        log.info("Refreshing Token")
-        access_token, expires_on, refresh_token, refresh_expires_on = gen.refresh_jwt_token(refresh_token)
+            log.info("Refreshing Token")
+            access_token, expires_on, refresh_token, refresh_expires_on = gen.refresh_jwt_token(refresh_token)
 
-        sleep_time = min(expires_on, refresh_expires_on) - 30 # 30 second buffer
+            sleep_time = max(60, min(expires_on, refresh_expires_on) - 120) # 2 minute buffer, wait atleast 60 seconds
 
-        log.info("Access Token Refreshed - new sleep time %d - %s" % (sleep_time, access_token))
-        secret_data = gen.generate(access_token, refresh_token, 'users-bbsae-xyz', user_project_id)
-        install_files (secret_data)
-        
-    except KeyboardInterrupt:
-        log.error("Keyboard Interrupted.  Exiting..")
-        sys.exit(1)
-    except:
-        log.error("Unexpected error")
-        log.error(str(sys.exc_info()))
-        tb = traceback.format_exc()
-        log.error(str(tb))
+            log.info("Access Token Refreshed - new sleep time %d - %s" % (sleep_time, access_token))
+            secret_data = gen.generate(access_token, refresh_token, 'users-bbsae-xyz', user_project_id)
+            install_files (secret_data)
+            
+        except KeyboardInterrupt:
+            log.error("Keyboard Interrupted.  Exiting..")
+            sys.exit(1)
+        except:
+            log.error("Unexpected error")
+            log.error(str(sys.exc_info()))
+            tb = traceback.format_exc()
+            log.error(str(tb))
+            errors = errors + 1
 
     log.info("Sleeping %d minutes..." % int(sleep_time/60))
     time.sleep(sleep_time)
