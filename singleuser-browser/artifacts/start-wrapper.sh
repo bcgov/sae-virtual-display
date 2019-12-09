433d38ea65c10b924b32e9f7803448e5986a476b
Ike The Coder <ikethecoder@canzea.com>
2019-12-09 12:19:21 -0800

    Browser fix

--- singleuser-browser/artifacts/start-wrapper.sh
+++ singleuser-browser/artifacts/start-wrapper.sh
@@ -4,7 +4,7 @@ set -x
 
 Xvfb +extension GLX +extension Composite -dpi 96 -nolisten tcp -noreset -screen 0 5760x2560x24+32 -auth /home/jovyan/.Xauthority :100 &
 
-sleep 2
+sleep 5
 
 sh -c launcher.sh
 
