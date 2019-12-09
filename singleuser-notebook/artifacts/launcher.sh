8fa3a4355c174d5e070f2678f991aef469c9a1a4
Ike The Coder <ikethecoder@canzea.com>
2019-12-09 09:17:01 -0800

    Update travis pipeline and chrome for notebook

--- singleuser-notebook/artifacts/launcher.sh
+++ singleuser-notebook/artifacts/launcher.sh
@@ -11,5 +11,5 @@ unset JPY_API_TOKEN
 
 JUPYTER_ENABLE_LAB=1 tini -g -- start-notebook.sh --port=4444 &
 
-google-chrome-stable --enable-logging --v=1 --no-first-run --no-default-browser-check --disable-background-networking --disable-breakpad --disable-cloud-import --disable-default-apps --disable-demo-mode --disable-device-discovery-notifications --disable-domain-reliability --disable-extensions --disable-file-system --disable-gpu --disable-in-process-stack-traces --disable-ios-physical-web --disable-login-animations --disable-print-preview --start-maximized --homepage ${HOME_PAGE} ${JHUB_URL}/user/$JUPYTERHUB_USER
+google-chrome-stable --no-sandbox --enable-logging --v=1 --no-first-run --no-default-browser-check --disable-background-networking --disable-breakpad --disable-cloud-import --disable-default-apps --disable-demo-mode --disable-device-discovery-notifications --disable-domain-reliability --disable-extensions --disable-file-system --disable-gpu --disable-in-process-stack-traces --disable-ios-physical-web --disable-login-animations --disable-print-preview --start-maximized --homepage ${HOME_PAGE} ${JHUB_URL}/user/$JUPYTERHUB_USER
 
