8d5acba1e1423f7a3c1af0533522265de2a469c1
ikethecoder <ikethecoder@canzea.com>
2020-01-09 15:31:52 -0800

    Upgrade rstudio with hadoop and aws s3 libs

--- singleuser-rstudio/artifacts/launcher.sh
+++ singleuser-rstudio/artifacts/launcher.sh
@@ -4,6 +4,7 @@
 # so that the Spark workers can communicate with the driver running in POD
 export SPARK_DRIVER_HOST=`hostname -I | xargs`
 export SPARK_MASTER_URL="spark://spark-master-svc:7077"
+export SPARK_DIST_CLASSPATH=$(hadoop classpath)
 
 alias s3sync='mc mirror minio/ /home/jovyan/work/ProjectGroupShare'
 
