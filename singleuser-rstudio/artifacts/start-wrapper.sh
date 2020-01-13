4ff0fc5b03df690e2662ec8991d7ddf0b923d2bc
ikethecoder <ikethecoder@canzea.com>
2020-01-13 11:09:28 -0800

    Disable GLX on X server

--- singleuser-rstudio/artifacts/start-wrapper.sh
+++ singleuser-rstudio/artifacts/start-wrapper.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-Xvfb +extension GLX +extension Composite -dpi 96 -nolisten tcp -noreset -screen 0 5760x2560x24+32 -auth /home/jovyan/.Xauthority :100 &
+Xvfb -extension GLX +extension Composite -dpi 96 -nolisten tcp -noreset -screen 0 5760x2560x24+32 -auth /home/jovyan/.Xauthority :100 &
 
 sleep 2
 
