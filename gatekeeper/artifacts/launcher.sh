d87565d87993bb7cefe6003c8e8bf46f7f1dc7b1
ikethecoder <ikethecoder@canzea.com>
2020-01-24 15:28:11 -0800

    Fix the keyboard layer to us to avoid issues with canada

--- gatekeeper/artifacts/launcher.sh
+++ gatekeeper/artifacts/launcher.sh
@@ -19,7 +19,7 @@ export XPRA_PRINTERS_GENERIC=0
 export XPRA_CUPS_DBUS=0
 export XPRA_SCALING=0
 
-xpra start :100 $XPRA_ARGS --bind-tcp=127.0.0.1:5000 --start-new-commands=no --resize-display=yes --fake-xinerama=no --pulseaudio=off --printing=no --mdns=no --webcam=no --clipboard=no --file-transfer=no --open-files=no --pulseaudio=no --speaker=off --printing=no --microphone=off --html=on
+xpra start :100 $XPRA_ARGS --bind-tcp=127.0.0.1:5000 --start-new-commands=no --resize-display=yes --fake-xinerama=no --pulseaudio=off --printing=no --mdns=no --webcam=no --clipboard=no --file-transfer=no --open-files=no --pulseaudio=no --speaker=off --printing=no --microphone=off --keyboard-layout=us --html=on
 
 sleep 2
 
