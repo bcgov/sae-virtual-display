2343feb8e39c44d2a024b4345226ff81aec5d3d8
Joshua Jones <joshua@general-metrics.com>
2020-02-19 15:09:13 -0800

    Add body arguments to the server start call.

--- hub/ui/src/hooks/use-server.js
+++ hub/ui/src/hooks/use-server.js
@@ -1,11 +1,13 @@
 import { useContext, useState } from 'react';
+import head from 'lodash/head';
 import WorkbenchContext from '@src/utils/context';
 
 function useServer({ app }) {
-  const { DEV, user } = useContext(WorkbenchContext);
+  const { DEV, projects, user } = useContext(WorkbenchContext);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
+  const project = head(projects);
 
   async function fetchData() {
     const controller = new AbortController();
@@ -20,6 +22,10 @@ function useServer({ app }) {
       const res = await fetch(url, {
         signal,
         method: 'POST',
+        body: JSON.stringify({
+          image: app,
+          project,
+        }),
       });
 
       setLoading(false);
