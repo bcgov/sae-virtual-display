bc2ba854e119b943c9ba0187779ed42512075126
ikethecoder <ikethecoder@canzea.com>
2020-04-03 14:52:03 -0700

    Upgrade ubuntu and no-sandbox (#13)

--- singleuser/singleuser-browser/artifacts/launcher.sh
+++ singleuser/singleuser-browser/artifacts/launcher.sh
@@ -4,5 +4,5 @@
 
 set -e
 
-DISPLAY=:100 google-chrome-stable --enable-logging --v=1 --no-first-run --no-default-browser-check --disable-background-networking --disable-breakpad --disable-cloud-import --disable-default-apps --disable-demo-mode --disable-device-discovery-notifications --disable-domain-reliability --disable-extensions --disable-file-system --disable-gpu --disable-in-process-stack-traces --disable-ios-physical-web --disable-login-animations --disable-print-preview --start-maximized --homepage ${HOME_PAGE} ${JHUB_URL}/user/$JUPYTERHUB_USER
+DISPLAY=:100 google-chrome-stable --no-sandbox --disable-dev-shm-usage --enable-logging --v=1 --no-first-run --no-default-browser-check --disable-background-networking --disable-breakpad --disable-cloud-import --disable-default-apps --disable-demo-mode --disable-device-discovery-notifications --disable-domain-reliability --disable-extensions --disable-file-system --disable-gpu --disable-in-process-stack-traces --disable-ios-physical-web --disable-login-animations --disable-print-preview --start-maximized --homepage ${HOME_PAGE} ${JHUB_URL}/user/$JUPYTERHUB_USER
 # --enable-logging  --kiosk  --start-maximized --start-maximized 
