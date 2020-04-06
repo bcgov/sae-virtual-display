98f7ee539183d743820ca2c1b2c8ee548a7c216b
ikethecoder <ikethecoder@canzea.com>
2020-04-06 16:03:56 -0700

    Upd notebook

--- singleuser/singleuser-notebook/artifacts/launcher.sh
+++ singleuser/singleuser-notebook/artifacts/launcher.sh
@@ -9,7 +9,9 @@ unset JUPYTERHUB_ADMIN_ACCESS
 unset JUPYTERHUB_API_TOKEN
 unset JPY_API_TOKEN
 
+echo 'alias s3="aws s3 --endpoint $MINIO_ADDR"' >> /home/jovyan/.profile
+
 JUPYTER_ENABLE_LAB=1 tini -g -- start-notebook.sh --port=4444 &
 
-DISPLAY=:100 google-chrome-stable --no-sandbox --enable-logging --v=1 --no-first-run --no-default-browser-check --disable-background-networking --disable-breakpad --disable-cloud-import --disable-default-apps --disable-demo-mode --disable-device-discovery-notifications --disable-domain-reliability --disable-extensions --disable-file-system --disable-gpu --disable-in-process-stack-traces --disable-ios-physical-web --disable-login-animations --disable-print-preview --start-maximized --homepage ${HOME_PAGE} ${JHUB_URL}/user/$JUPYTERHUB_USER
+DISPLAY=:100 google-chrome-stable --no-sandbox --disable-dev-shm-usage --enable-logging --v=1 --no-first-run --no-default-browser-check --disable-background-networking --disable-breakpad --disable-cloud-import --disable-default-apps --disable-demo-mode --disable-device-discovery-notifications --disable-domain-reliability --disable-extensions --disable-file-system --disable-gpu --disable-in-process-stack-traces --disable-ios-physical-web --disable-login-animations --disable-print-preview --start-maximized --homepage ${HOME_PAGE} ${JHUB_URL}/user/$JUPYTERHUB_USER
 
