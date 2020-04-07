ad68c3bb92a06bb5e4b0129641f2e8358d803c35
ikethecoder <ikethecoder@canzea.com>
2020-04-07 09:56:21 -0700

    notebook inc token

--- singleuser/singleuser-notebook/artifacts/launcher.sh
+++ singleuser/singleuser-notebook/artifacts/launcher.sh
@@ -11,9 +11,12 @@ unset JPY_API_TOKEN
 
 echo 'alias s3="aws s3 --endpoint $MINIO_ADDR"' >> /home/jovyan/.profile
 
+NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+echo "c.NotebookApp.token = u'${NEW_UUID}'" >> ~/.jupyter/jupyter_notebook_config.py
+
 JUPYTER_ENABLE_LAB=1 tini -g -- start-notebook.sh --port=4444 &
 
 sleep 3
 
-DISPLAY=:100 google-chrome-stable --no-sandbox --disable-dev-shm-usage --enable-logging --v=1 --no-first-run --no-default-browser-check --disable-background-networking --disable-breakpad --disable-cloud-import --disable-default-apps --disable-demo-mode --disable-device-discovery-notifications --disable-domain-reliability --disable-extensions --disable-file-system --disable-gpu --disable-in-process-stack-traces --disable-ios-physical-web --disable-login-animations --disable-print-preview --start-maximized --homepage http://localhost:4444/lab
+DISPLAY=:100 google-chrome-stable --no-sandbox --disable-dev-shm-usage --enable-logging --v=1 --no-first-run --no-default-browser-check --disable-background-networking --disable-breakpad --disable-cloud-import --disable-default-apps --disable-demo-mode --disable-device-discovery-notifications --disable-domain-reliability --disable-extensions --disable-file-system --disable-gpu --disable-in-process-stack-traces --disable-ios-physical-web --disable-login-animations --disable-print-preview --start-maximized --homepage http://localhost:4444/lab?token=$NEW_UUID
 
