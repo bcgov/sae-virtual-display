62d9f8ede5211e25dc0c503847646c0d559ad821
ikethecoder <ikethecoder@canzea.com>
2020-04-06 17:02:43 -0700

    notebook specific start page

--- singleuser/singleuser-notebook/artifacts/launcher.sh
+++ singleuser/singleuser-notebook/artifacts/launcher.sh
@@ -13,5 +13,7 @@ echo 'alias s3="aws s3 --endpoint $MINIO_ADDR"' >> /home/jovyan/.profile
 
 JUPYTER_ENABLE_LAB=1 tini -g -- start-notebook.sh --port=4444 &
 
-DISPLAY=:100 google-chrome-stable --no-sandbox --disable-dev-shm-usage --enable-logging --v=1 --no-first-run --no-default-browser-check --disable-background-networking --disable-breakpad --disable-cloud-import --disable-default-apps --disable-demo-mode --disable-device-discovery-notifications --disable-domain-reliability --disable-extensions --disable-file-system --disable-gpu --disable-in-process-stack-traces --disable-ios-physical-web --disable-login-animations --disable-print-preview --start-maximized --homepage ${HOME_PAGE} ${JHUB_URL}/user/$JUPYTERHUB_USER
+sleep 3
+
+DISPLAY=:100 google-chrome-stable --no-sandbox --disable-dev-shm-usage --enable-logging --v=1 --no-first-run --no-default-browser-check --disable-background-networking --disable-breakpad --disable-cloud-import --disable-default-apps --disable-demo-mode --disable-device-discovery-notifications --disable-domain-reliability --disable-extensions --disable-file-system --disable-gpu --disable-in-process-stack-traces --disable-ios-physical-web --disable-login-animations --disable-print-preview --start-maximized --homepage http://localhost:4444/lab
 
